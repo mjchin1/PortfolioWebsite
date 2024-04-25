76184496c6bf448a281d8c13cba0d863c1e57271
Melissa J. Chin <melissa1jchin@gmail.com>
2024-04-25 17:29:12 -0500

    removed unused imports

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -6,10 +6,8 @@ import FieldtripSinglePage from '../assets/FieldtripSinglePage.png'
 import FieldtripFavoritesPage from '../assets/FieldtripFavoritesPage.png'
 import ReviveHomepage from '../assets/ReviveHomepage.png'
 import ReviveCalendar from '../assets/ReviveCalendar.png'
-import ReviveCalendarForm from '../assets/ReviveCalendarForm.png'
 import ReviveSchedule from '../assets/ReviveSchedule.png'
 import ReviveAccountPage from '../assets/ReviveAccountPage.png'
-import ReviveAccountPage2 from '../assets/ReviveAccountPage2.png'
 
 
 
@@ -25,13 +23,13 @@ export default function Projects() {
     <img className="projectPhoto" src={MoodboardLogin}/>
     <img className="projectPhoto" src={MoodboardWebsite}/>
     <p>
-    React.js · Node.js · PostgreSQL · Express.js <br/> <br></br>
+    React.js · Vite · Node.js · PostgreSQL · Express.js <br/> <br></br>
 
     The idea for Moodboard came from my experience with using Squarespace during my time as a creative entrepreneur. Moodboard is a full-stack application that uses form data submitted by users to design a beautiful website. Users can create a website without logging in. Once the user creates an account, they gain the functionality to save and edit the websites they have created. <br/>
     <br/>Deployed App · GitHub Repository
 
     <div className="breakline"></div>
-    
+
     </p>
     <h2>The Field Trip</h2>
     <h3>A Full-Stack Directory of Chicago's Hidden Gems</h3>
@@ -39,7 +37,7 @@ export default function Projects() {
     <img className="projectPhoto" src={FieldtripSinglePage}/>
     <img className="projectPhoto" src={FieldtripFavoritesPage}/>
     <p>
-    React.js · Node.js · PostgreSQL · Express.js · Redux · RTK Query <br/> <br/>
+    React.js · Vite · Node.js · PostgreSQL · Express.js · Redux · RTK Query <br/> <br/>
 
     The Field Trip is a directory of hidden gems and unique spaces in Chicago. I created this full-stack application for people who are looking to break out of their routines and explore new parts of the city. The application allows users to view the entire directory without creating an account. Once they create an account, users gain the functionality to save individual locations to their list of favorite places, submit their own favorite places to be featured in the directory, and delete the places that they have submitted. <br/>
 
@@ -57,7 +55,7 @@ export default function Projects() {
     <img className="reviveProjectPhoto" src={ReviveAccountPage}/>
     </div>
     <p>
-    React.js · Node.js · PostgreSQL · Express.js · Redux · RTK Query · Bootstrap <br/> <br/>
+    React.js · Vite · Node.js · PostgreSQL · Express.js · Redux · RTK Query · Bootstrap <br/> <br/>
 
     My teammates and I created Revive as our capstone project for the Grace Hopper Program. Revive is a wellness app that helps users explore and save fitness routines, healthy recipes, and self-care ideas while allowing them to schedule and track their routines with a calendar. I created the calendar component of this app, which allows logged-in users to see a full monthly calendar view and add or delete events for individual calendar days. I also created the “Account” component of the app, which ties all of the app’s sections together by rendering the user’s saved fitness routines, recipes, self-care ideas, and daily schedule all in one place.  <br/>
 
