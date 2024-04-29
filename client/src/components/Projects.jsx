eb2ec07ae1fbdd3213840c25bd66ea1b6735c9b5
Melissa J. Chin <melissa1jchin@gmail.com>
2024-04-29 15:28:02 -0500

    minor description edit

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -40,7 +40,7 @@ export default function Projects() {
     <p>
     React.js · Vite · Node.js · PostgreSQL · Express.js · Redux · RTK Query <br/> <br/>
 
-    The Field Trip is a directory of hidden gems and unique spaces in Chicago. Sometimes we need little a push to shake up our routines and open ourselves up to new experiences. I created this application for Chicagoans who are looking to have new adventures by exploring beautiful, unfamiliar locations throughout the city. The application allows users to view the entire directory without creating an account, but once they do create an account, they gain the functionality to save individual locations to their list of favorite places, submit their own favorite places to be featured in the directory, and delete the places that they have submitted to the directory.  <br/>
+    The Field Trip is a directory of hidden gems and unique spaces in Chicago. Sometimes we need little a push to shake up our routines and open ourselves up to new experiences. I created this application for Chicagoans who are looking to have new adventures by exploring beautiful, unfamiliar locations throughout the city. The application allows users to view the entire directory without creating an account, but once they do create an account, they gain the functionality to save individual locations to their list of favorite places, submit their own favorite places to the directory database, and delete the places that they have submitted.  <br/>
 
    <br/> Deployed App · GitHub Repository
     </p>
