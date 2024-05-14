71e3e1f772fc1da9b43e0ef997814345109444a0
Melissa J. Chin <melissa1jchin@gmail.com>
2024-05-13 22:04:06 -0500

    added live demo hyperlink for Moodboard

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -27,7 +27,7 @@ export default function Projects() {
     React.js · Vite · Node.js · PostgreSQL · Express.js <br/> <br></br>
 
     Moodboard is a full-stack application that uses form data submitted by users to instantly design a beautiful website. While I love experimenting with design, I understand that making design decisions can be a huge hassle for many people when creating a website. Moodboard takes these decisions off the user’s plate, organizing their information and images into a universally flattering format. While users can create a website without logging in, once they create an account, they gain the functionality to save and edit the websites they have created.  <br/>
-    <br/> <a className="appLink" href="" target="_blank">Live Demo</a> · <a className="appLink" href="https://moodboardwebsitebuilder.onrender.com" target="_blank">Deployed App</a> · <a className="appLink"  href="https://github.com/mjchin1/Moodboard-WebsiteBuilder" target="_blank">GitHub Repository </a>
+    <br/> <a className="appLink" href="https://youtu.be/Wcr2v1Hi3vI" target="_blank">Live App Demo</a> · <a className="appLink" href="https://moodboardwebsitebuilder.onrender.com" target="_blank">Deployed App</a> · <a className="appLink"  href="https://github.com/mjchin1/Moodboard-WebsiteBuilder" target="_blank">GitHub Repository </a>
 
     <div className="breakline"></div>
 
@@ -42,7 +42,7 @@ export default function Projects() {
 
     The Field Trip is a directory of hidden gems and unique spaces in Chicago. Sometimes we need little a push to shake up our routines and open ourselves up to new experiences. I created this application for Chicagoans who are looking to have new adventures by exploring beautiful, unfamiliar locations throughout the city. The application allows users to view the entire directory without creating an account, but once they do create an account, they gain the functionality to save individual locations to their list of favorite places, submit their own favorite places to the directory database, and delete the places that they have submitted.  <br/>
 
-   <br/> <a className="appLink" href="https://youtu.be/NQ1TmC6dGYo" target="_blank">Live Demo</a> · <a className="appLink" href="https://thefieldtrip.onrender.com" target="_blank">Deployed App</a> · <a className="appLink" href="https://github.com/mjchin1/TheFieldTrip-ChicagoDirectory" target="_blank">GitHub Repository </a>
+   <br/> <a className="appLink" href="https://youtu.be/NQ1TmC6dGYo" target="_blank">Live App Demo</a> · <a className="appLink" href="https://thefieldtrip.onrender.com" target="_blank">Deployed App</a> · <a className="appLink" href="https://github.com/mjchin1/TheFieldTrip-ChicagoDirectory" target="_blank">GitHub Repository </a>
     </p>
     <div className="breakline"></div>
 
@@ -61,7 +61,7 @@ export default function Projects() {
 
     My teammates and I created Revive as our capstone project for the Grace Hopper Program. Revive is a wellness app that helps users explore and save fitness routines, healthy recipes, and self-care ideas while allowing them to schedule and track their routines with a calendar. I created the calendar component of this app, which allows logged-in users to see a full monthly calendar view, add events for specific days using a scheduling form, see their daily schedule, and delete events.  <br/>
 
-    <br/> <br/> <a className="appLink" href="" target="_blank">Live Demo</a> · <a className="appLink" href="https://revive-app.onrender.com" target="_blank">Deployed App (please use Google Chrome for best user experience)</a> · <a className="appLink" href="https://github.com/revive-team1/revive-capstone" target="_blank">GitHub Repository </a>
+    <br/> <br/> <a className="appLink" href="" target="_blank">Live App Demo</a> · <a className="appLink" href="https://revive-app.onrender.com" target="_blank">Deployed App (please use Google Chrome for best user experience)</a> · <a className="appLink" href="https://github.com/revive-team1/revive-capstone" target="_blank">GitHub Repository </a>
     </p>
 
 
